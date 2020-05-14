a6a00da686350b8a6df8e5c109e0ebaf60b5532b
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2020-05-15 02:44:28 +0400

    Add more methods to Gameboard class

--- Gameboard.py
+++ Gameboard.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-
 class Gameboard:
     # static variables (they aren't intended to change)
     nrows, ncols = 10, 10
@@ -41,39 +37,78 @@ class Gameboard:
         self.ax.set_xbound(-0.5, 9.5)
         self.ax.set_ybound(-0.5, 9.5)
         
+        # initialize counters
+        self.alive_boat_count = 0
+        self.shot_count = 0
+        self.hit_count = 0
+        
     # position should be a string or list of strings in [A-J][1-10] format
     def hide_ship(self, *locations):
         for location in locations:
+            self.alive_boat_count += 1
             row = ord(location[0]) - ord('A')
             col = int(location[1:]) - 1
             self.board[row, col] = Gameboard.boat_color
             
     def unhide_ship(self, *locations):
         for location in locations:
+            self.alive_boat_count -= 1
             row = ord(location[0]) - ord('A')
             col = int(location[1:]) - 1
             # change square color to white
             self.board[row, col] = 1
     
     def clear(self):
+        self.alive_boat_count = 0
+        self.shot_count = 0
+        self.hit_count = 0
         self.board = np.ones([Gameboard.nrows, Gameboard.ncols, 3])
         for line in self.ax.get_lines():
             line.set_marker(None)
         
     # updates gameboard and returns True if boat was hit
     def shoot(self, location):
+        self.shot_count += 1
+        
         row = ord(location[0]) - ord('A')
         col = int(location[1:]) - 1
 
         # draw grey hit marker
         self.ax.plot(col, row, marker='X', markersize=15, color='darkgray')
 
-        # check if boat's at current location and change the color of square to red if so
+        # check if boat's at current location and change the color of square to red (destroy ship) if so
         if (self.board[row, col] == Gameboard.boat_color).all():
             self.board[row, col] = Gameboard.hit_color
+            self.alive_boat_count -= 1
+            self.hit_count += 1
             return True
         else:
             return False
+        
+    # returns True if all ships are destroyed
+    def is_game_over(self):
+        if self.alive_boat_count == 0:
+            return True
+        return False
+    
+    # returns number of boats currently alive
+    def get_alive_count(self):
+        return self.alive_boat_count
+    
+    # returns how many shots have been made so far
+    def get_shot_count(self):
+        return self.shot_count
+    
+    # returns how many ships are hit
+    def get_hit_count(self):
+        return self.hit_count
+    
+    # returns accuracy (# of hits) / (# of shots)
+    def get_accuracy(self):
+        if self.shot_count == 0:
+            return 0
+        else:
+            return self.hit_count / self.shot_count
         
     # returns matplotlib figure
     def get_figure(self):
